85da7ffb9dfb73aaf25f99ec30b12555981e8250
Norman Nexo <normannexo@gmail.com>
2020-03-18 21:06:21 +0100

    structure changed

--- clean_archive.py
+++ clean_archive.py
@@ -18,7 +18,7 @@ for file in files:
         df['deaths'] = df.confirmed.str.extract(r'\((\d+)\)')
         df['confirmed']=df.confirmed.str.replace(r'(\d+) \(\d+\)',r'\1')
     df = df[['Bundesland','date', 'confirmed', 'deaths']]
-    df.to_csv(os.path.join(destpath, filename))
+    df.to_csv(os.path.join(destpath, filename), index=False)
 
 
 
